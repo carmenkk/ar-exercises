3db2de52e55934ae3ebee82c11fac0949dae59fe
Carmen Kuang <carmen@Carmens-MacBook-Air.local>
2022-04-05 23:02:10 -0400

    create employees in specific store

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Karen", last_name: "Chen", hourly_rate: 30)
+@store2.employees.create(first_name: "Joe", last_name: "Green", hourly_rate: 45)
+@store2.employees.create(first_name: "Rachel", last_name: "Viran", hourly_rate: 35)
+@store2.employees.create(first_name: "Mark", last_name: "Vii", hourly_rate: 50)
 # Your code goes here ...
